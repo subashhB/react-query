945878e447907dd3de6a27587770640c5a6f96a6
Admin <subashbaidya9@gmail.com>
2023-04-26 15:22:10 +0545

    Query Variables

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import Planet from './Planet';
 
-const fetchPlanets = async()=>{
-  const res = await fetch('http://swapi.dev/api/planets/');
+const fetchPlanets = async(key, greetings,page)=>{
+  console.log(greetings)
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json()
 }
 
 const Planets = () => {
-  const {data, status} = useQuery({queryKey: ['planets'], queryFn: fetchPlanets, staleTime: 0, onSuccess: ()=>{console.log('Data fetched with no Problem')}})
+  const[page, setPage]= useState(1);
+  const variable = 'Hello Voyager'
+  const {data, status} = useQuery({queryKey: ['planets',variable, page], queryFn:()=> fetchPlanets(null,variable,page)})
   console.log(data)
   return (
     <div>
+        <button onClick={()=>setPage(1)} >Page 1</button>
+        <button onClick={()=>setPage(2)} >Page 2</button>
+        <button onClick={()=>setPage(3)} >Page 3</button>
         <h2>Planets</h2>
         { status === 'error' && (
           <div>Error Fetching the data</div>

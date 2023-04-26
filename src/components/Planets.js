36208bed62c26edae1d3cb10e8184d71852c2954
Admin <subashbaidya9@gmail.com>
2023-04-26 19:21:06 +0545

    Pagination

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -2,22 +2,17 @@ import React, { useState } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import Planet from './Planet';
 
-const fetchPlanets = async(key, greetings,page)=>{
-  console.log(greetings)
+const fetchPlanets = async(page =1)=>{
   const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
   return res.json()
 }
 
 const Planets = () => {
   const[page, setPage]= useState(1);
-  const variable = 'Hello Voyager'
-  const {data, status} = useQuery({queryKey: ['planets',variable, page], queryFn:()=> fetchPlanets(null,variable,page)})
+  const {data, isPreviousData, isFetching, status} = useQuery({queryKey: ['planets', page], queryFn: ()=> fetchPlanets(page)})
   console.log(data)
   return (
     <div>
-        <button onClick={()=>setPage(1)} >Page 1</button>
-        <button onClick={()=>setPage(2)} >Page 2</button>
-        <button onClick={()=>setPage(3)} >Page 3</button>
         <h2>Planets</h2>
         { status === 'error' && (
           <div>Error Fetching the data</div>
@@ -26,7 +21,18 @@ const Planets = () => {
           <div>Loading data...</div>
         )}
         {status === 'success' && (
-          <div>{data.results.map(planet => <Planet key={planet.name} planet={planet} /> )}</div>
+          <>
+            <button
+              onClick={() => setPage(old => Math.max(old - 1 , 1))}
+              disabled={page === 1}
+            >Previous Page</button>
+            { page }
+            <button
+              onClick={()=>setPage(old => (!isFetching && !isPreviousData && !data.next ? old: old+1 ))}
+              disabled={!isFetching && !isPreviousData && !data.next}
+            >Next Page</button>
+            <div>{data.results.map(planet => <Planet key={planet.name} planet={planet} /> )}</div>
+          </>
         )
 
         }
